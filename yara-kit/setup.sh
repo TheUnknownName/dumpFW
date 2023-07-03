33062c41266a0bc8022aba2913152c30b7b599e9
TUNBudi06 <anjaytun2006@gmail.com>
2023-07-03 17:57:04 +0700

    dump:run dump by yara 2

--- yara-kit/setup.sh
+++ yara-kit/setup.sh
@@ -1,30 +1,100 @@
 #!/bin/bash
 
-# Determine which command to use for privilege escalation
-if command -v sudo > /dev/null 2>&1; then
-    sudo_cmd="sudo"
-elif command -v doas > /dev/null 2>&1; then
-    sudo_cmd="doas"
-else
-    echo "Neither sudo nor doas found. Please install one of them."
+# Clear Screen
+tput reset 2>/dev/null || clear
+
+# Colours (or Colors in en_US)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
+
+# Abort Function
+function abort(){
+    [ ! -z "$@" ] && echo -e ${RED}"${@}"${NORMAL}
     exit 1
-fi
+}
+
+# Banner
+function __bannerTop() {
+	echo -e \
+	${GREEN}"
+	██████╗░██╗░░░██╗███╗░░░███╗██████╗░██████╗░██╗░░██╗
+	██╔══██╗██║░░░██║████╗░████║██╔══██╗██╔══██╗╚██╗██╔╝
+	██║░░██║██║░░░██║██╔████╔██║██████╔╝██████╔╝░╚███╔╝░
+	██║░░██║██║░░░██║██║╚██╔╝██║██╔═══╝░██╔══██╗░██╔██╗░
+	██████╔╝╚██████╔╝██║░╚═╝░██║██║░░░░░██║░░██║██╔╝╚██╗
+	╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝
+	"${NC}
+}
+
+# Welcome Banner
+printf "\e[32m" && __bannerTop && printf "\e[0m"
+
+# Minor Sleep
+sleep 1
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
+
     if [[ "$(command -v apt)" != "" ]]; then
-        $sudo_cmd apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl python3-venv -y
+
+        echo -e ${PURPLE}"Ubuntu/Debian Based Distro Detected"${NORMAL}
+        sleep 1
+        echo -e ${BLUE}">> Updating apt repos..."${NORMAL}
+        sleep 1
+	    sudo apt -y update || abort "Setup Failed!"
+	    sleep 1
+	    echo -e ${BLUE}">> Installing Required Packages..."${NORMAL}
+	    sleep 1
+        sudo apt install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev jq || abort "Setup Failed!"
+
     elif [[ "$(command -v dnf)" != "" ]]; then
-        $sudo_cmd dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
+
+        echo -e ${PURPLE}"Fedora Based Distro Detected"${NORMAL}
+        sleep 1
+	    echo -e ${BLUE}">> Installing Required Packages..."${NORMAL}
+	    sleep 1
+
+	    # "dnf" automatically updates repos before installing packages
+        sudo dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins || abort "Setup Failed!"
+
     elif [[ "$(command -v pacman)" != "" ]]; then
-        $sudo_cmd pacman -Sy --noconfirm --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4
+
+        echo -e ${PURPLE}"Arch or Arch Based Distro Detected"${NORMAL}
+        sleep 1
+	    echo -e ${BLUE}">> Installing Required Packages..."${NORMAL}
+	    sleep 1
+
+        sudo pacman -Syyu --needed --noconfirm 2>&1 | grep -v "warning: could not get file information" || abort "Setup Failed!"
+        sudo pacman -Sy --noconfirm unace unrar p7zip sharutils uudeview arj cabextract file-roller dtc brotli axel gawk aria2 detox cpio lz4 jq || abort "Setup Failed!"
+
+        # Python
+        sleep 1
+        echo -e ${BLUE}">> Creating Required Python3 Symlinks..."${NORMAL}
+        sleep 1
+
     fi
-    PIP=pip3
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install protobuf xz brotli lz4 aria2 detox coreutils p7zip gawk
-    PIP=pip
+
+    echo -e ${PURPLE}"macOS Detected"${NORMAL}
+    sleep 1
+	echo -e ${BLUE}">> Installing Required Packages..."${NORMAL}
+	sleep 1
+    brew install protobuf xz brotli lz4 aria2 detox coreutils p7zip gawk || abort "Setup Failed!"
+
 fi
 
-# Create virtual environment and install packages
-python3 -m venv .venv
-source .venv/bin/activate
-"$PIP" install aospdtgen backports.lzma extract-dtb protobuf pycrypto docopt zstandard
+sleep 1
+echo -e ${PURPLE}"Distro Specific Setup Done, Now Installing pyhton Packages from pip..."${NORMAL}
+sleep 1
+sudo pip install backports.lzma extract-dtb protobuf==3.20.0 pycryptodome docopt zstandard twrpdtgen future requests humanize clint lz4 pycryptodome pycryptodomex || abort "Setup Failed!"
+sleep 1
+pip install git+https://github.com/sebaubuntu-python/aospdtgen || abort "Setup Failed!"
+
+# Done!
+echo -e ${GREEN}"Setup Complete!"${NORMAL}
+
+# Exit
+exit 0
